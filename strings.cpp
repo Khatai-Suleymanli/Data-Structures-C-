1e4653eef7431cf15c24f1623100c5a160208670
Khatai Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2022-12-01 20:36:15 +0400

    strings.cpp

---
+++ strings.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// strings are also dynamic arrays as vectors, but they
+// substr(x,y) returns the elements of the string in the given interval
+
+int main(){
+	string a = "Azerbaijan";
+	string b = "Republic";
+	
+	string c = a+b; 
+	cout<<a.substr(1,5)<<"\n"; 
+}
